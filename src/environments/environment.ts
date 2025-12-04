c0a48319cfba8f107dd75d92ad2e30abc159c6f6
Walid Lamkharbech (Desktop) <walid.lamkharbech@gmail.com>
2025-12-04 01:09:55 +0100

    Changement URL par défaut

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 // Environnement de développement (local)
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080'
+  apiUrl: 'https://api-iot.sentori-studio.com'
 };
 
